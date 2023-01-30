1e40296c580fdc794507555784432b6fe5f60cda
yash22210 <110306718+yash22210@users.noreply.github.com>
2023-01-30 16:03:17 -0500

    Add files via upload
    Integrate a web mapping service into an application
    Use the Geolocation API to integrate user location into a web application
    Use a Geocoding API to determine GPS co-ordinates from address information

---
+++ Google_map/Assignment1/js/Arenas.js
@@ -0,0 +1,428 @@
+var Arena = [{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 1,
+        "NAME": "Beverly Arena",
+        "ADDRESS": "680 Hwy 8, Rockton, ON L0R 1X0",
+        "COMMUNITY": "Flamborough",
+        "PHONE": "519-647-2722",
+        "LONGITUDE": "-80.11132698230543",
+        "LATITUDE": "43.29850975800341",
+        "Image": "<img width='200' height='150'style='border: 2px solid black'src='img/Beverly.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-80.111326982305357, 43.298509757066789]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 2,
+        "NAME": "Bill Friday Lawfield Arena",
+        "ADDRESS": "150 Folkstone Avenue",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-546-4923",
+        "LONGITUDE": "-79.85075118048759",
+        "LATITUDE": "43.21484012102844",
+        "Image": "<img width='200' height='150' src='img/LAWFIELD.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.850751180487507, 43.214840120091999]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 3,
+        "NAME": "Carlisle Arena",
+        "ADDRESS": "1496 Centre Road",
+        "COMMUNITY": "Flamborough",
+        "PHONE": "905-689-7283",
+        "LONGITUDE": "-79.98131996076191",
+        "LATITUDE": "43.39677849605811",
+        "Image": "<img width='200' height='150' src='img/Carlisle.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.981319960761823, 43.396778495121332]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 4,
+        "NAME": "Chedoke Twin Pad Arena",
+        "ADDRESS": "91 Chedmac Drive",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-546-2429",
+        "LONGITUDE": "-79.92059943158375",
+        "LATITUDE": "43.238669620839",
+        "Image": "<img width='200' height='150' src='img/Chedoke.jpeg'>"
+        
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.920599431583668, 43.238669619902502]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 5,
+        "NAME": "Coronation Arena",
+        "ADDRESS": "81 Macklin Street North",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-546-3109",
+        "LONGITUDE": "-79.89626047530253",
+        "LATITUDE": "43.264644122018204",
+        "Image": "<img width='200' height='150' src='img/Coronation.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.896260475302441, 43.264644121081666]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 6,
+        "NAME": "Eastwood Arena",
+        "ADDRESS": "111 Burlington Street East",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-546-3152",
+        "LONGITUDE": "-79.85632979848626",
+        "LATITUDE": "43.27241885012524",
+        "Image": "<img width='200' height='150' src='img/Eastwood.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.856329798486158, 43.272418849188696]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 7,
+        "NAME": "F.H. Sherman Arena",
+        "ADDRESS": "388 First Road East",
+        "COMMUNITY": "Stoney Creek",
+        "PHONE": "905-560-5886",
+        "LONGITUDE": "-79.75438830976113",
+        "LATITUDE": "43.195661115669616",
+        "Image": "<img width='200' height='150' src='img/Sherman.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.75438830976104, 43.195661114733234]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 8,
+        "NAME": "FirstOntario Centre",
+        "ADDRESS": "101 York Boulevard",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-546-3100",
+        "LONGITUDE": "-79.87219966580358",
+        "LATITUDE": "43.259137916576634",
+        "Image": "<img width='200' height='150' src='img/FirstOntario.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.872199665803493, 43.259137915640103]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 9,
+        "NAME": "Gateway Ice Centre",
+        "ADDRESS": "610 South Service Road",
+        "COMMUNITY": "Stoney Creek",
+        "PHONE": "905-643-8668",
+        "LONGITUDE": "-79.69992693685201",
+        "LATITUDE": "43.22880902640576",
+        "Image": "<img width='200' height='150' src='img/Gateway.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.699926936851938, 43.228809025469332]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 10,
+        "NAME": "Glanbrook Arena",
+        "ADDRESS": "4300 Binbrook Road, Hamilton",
+        "COMMUNITY": "Glanbrook",
+        "PHONE": "905-692-9331",
+        "LONGITUDE": "-79.83870498186084",
+        "LATITUDE": "43.13003002915828",
+        "Image": "<img width='200' height='150' src='img/Glanbrook.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.838704981860758, 43.130030028221988]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 11,
+        "NAME": "Hamilton Doublerink Arena",
+        "ADDRESS": "1911 Barton Street East",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-545-5894",
+        "LONGITUDE": "-79.78249455214632",
+        "LATITUDE": "43.244581371539574",
+        "Image": "<img width='200' height='150' src='img/Doublerink.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.782494552146233, 43.244581370603079]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 12,
+        "NAME": "Harry Howell Arena",
+        "ADDRESS": "27 Highway No. 5 West",
+        "COMMUNITY": "Flamborough",
+        "PHONE": "905-689-6666",
+        "LONGITUDE": "-79.92278840417256",
+        "LATITUDE": "43.31308838386527",
+        "Image": "<img width='200' height='150' src='img/Howell.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.922788404172465, 43.313088382928633]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 13,
+        "NAME": "Inch Park Arena",
+        "ADDRESS": "400 Queensdale Avenue East",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-546-4922",
+        "LONGITUDE": "-79.85959604063545",
+        "LATITUDE": "43.23706516640044",
+        "Image": "<img width='200' height='150' src='img/InchPark.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.859596040635367, 43.237065165463946]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 14,
+        "NAME": "J.L. Grightmire (Market Street) Arena",
+        "ADDRESS": "35 Market Street South, Hamilton",
+        "COMMUNITY": "Dundas",
+        "PHONE": "905-540-6678",
+        "LONGITUDE": "-79.96252182382118",
+        "LATITUDE": "43.26601388087859",
+        "Image": "<img width='200' height='150' src='img/Grightmire.jpeg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.962521823821078, 43.266013879942037]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 15,
+        "NAME": "Mohawk 4 Ice Centre",
+        "ADDRESS": "710 Mountain Brow Boulevard",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-318-5111",
+        "LONGITUDE": "-79.81712556278215",
+        "LATITUDE": "43.20947254897485",
+        "Image": "<img width='200' height='150' src='img/Mohawk4.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.817125562782081, 43.209472548038413]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 16,
+        "NAME": "Morgan Firestone Arena",
+        "ADDRESS": "385 Jerseyville Road West",
+        "COMMUNITY": "Ancaster",
+        "PHONE": "905-546-3769",
+        "LONGITUDE": "-80.00761609714841",
+        "LATITUDE": "43.2180512046866",
+        "Image": "<img width='200' height='150' src='img/Morgan.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-80.007616097148329, 43.21805120375015]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 17,
+        "NAME": "Mountain (Dave Andreychuk) Arena",
+        "ADDRESS": "25 Hester Street",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-546-4938",
+        "LONGITUDE": "-79.88123418785996",
+        "LATITUDE": "43.225161871564936",
+        "Image": "<img width='200' height='150' src='img/Mountain.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.881234187859874, 43.225161870628455]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 18,
+        "NAME": "Pat Quinn Parkdale Arena",
+        "ADDRESS": "1770 Main Street East",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-546-4785",
+        "LONGITUDE": "-79.7937883720762",
+        "LATITUDE": "43.23633025906635",
+        "Image": "<img width='200' height='150' src='img/Parkdale.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.793788372076122, 43.236330258129883]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 19,
+        "NAME": "Rosedale Arena",
+        "ADDRESS": "100 Greenhill Avenue",
+        "COMMUNITY": "Hamilton",
+        "PHONE": "905-546-4805",
+        "LONGITUDE": "-79.80696013353241",
+        "LATITUDE": "43.218875869192146",
+        "Image": "<img width='200' height='150' src='img/Rosedale.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.806960133532328, 43.218875868255701]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 20,
+        "NAME": "Saltfleet Arena",
+        "ADDRESS": "24 Sherwood Park Road, canada",
+        "COMMUNITY": "Stoney Creek",
+        "PHONE": "905-643-3883",
+        "LONGITUDE": "-79.70484095074632",
+        "LATITUDE": "43.217960221311095",
+        "Image": "<img width='200' height='150' src='img/Saltfleet.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.704840950746217, 43.217960220374678]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 21,
+        "NAME": "Spring Valley Arena",
+        "ADDRESS": "29 Orchard Drive",
+        "COMMUNITY": "Ancaster",
+        "PHONE": "905-648-4404",
+        "LONGITUDE": "-79.99779878425197",
+        "LATITUDE": "43.21712082837575",
+        "Image": "<img width='200' height='150' src='img/SpringValley.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.997798784251884, 43.217120827439281]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 22,
+        "NAME": "Stoney Creek Arena",
+        "ADDRESS": "37 King Street West",
+        "COMMUNITY": "Stoney Creek",
+        "PHONE": "905-662-2426",
+        "LONGITUDE": "-79.76400580995063",
+        "LATITUDE": "43.216531898454555",
+        "Image": "<img width='200' height='150' src='img/StoneyCreek.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.764005809950532, 43.216531897518138]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 23,
+        "NAME": "Valley Park Arena",
+        "ADDRESS": "970 Paramount Drive",
+        "COMMUNITY": "Stoney Creek",
+        "PHONE": "905-546-2424 ext.1185",
+        "LONGITUDE": "-79.79798203739882",
+        "LATITUDE": "43.19284948296531",
+        "Image": "<img width='200' height='150' src='img/ValleyPark.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.797982037398754, 43.192849482028912]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 24,
+        "NAME": "Wentworth Arenas",
+        "ADDRESS": "1249 Wilson Street East",
+        "COMMUNITY": "Ancaster",
+        "PHONE": "905-525-6233",
+        "LONGITUDE": "-79.94595167051109",
+        "LATITUDE": "43.247834343121134"
+        , "Image": "<img width='200' height='150' src='img/WentworthArenas.jpg'>"
+
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.945951670511008, 43.247834342184611]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "OBJECTID": 25,
+        "NAME": "Westoby (Olympic) Arena",
+        "ADDRESS": "70 Olympic Drive",
+        "COMMUNITY": "Dundas",
+        "PHONE": "905-540-6686",
+        "LONGITUDE": "-79.93426208805114",
+        "LATITUDE": "43.272877230642095",
+        "Image": "<img width='200' height='150' src='img/WestobyArena.jpg'>"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-79.934262088051042, 43.272877229705522]
+    }
+}
+]               
